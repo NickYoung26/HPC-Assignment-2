3291acd2567bb31f6cfaa2332aca4d71382da4cd
Nicholas Young <qdb21131@archie-w.hpc.strath.ac.uk>
2026-02-12 19:05:54 +0000

    added read,write and execution permissions to the .py file. read,write and execution permissions to the .py file.
    Successfully created a vector class in the python script.

--- OOPclasses.py
+++ OOPclasses.py
@@ -3,11 +3,33 @@
 """
 Version: Python 3.10.9
 
-Description: Optimising parralesisation of an integral calculation equal to pi.
+Description: Using OOP classes to represent and manipulate cartesian vectors.
 
-Date: 02/02/2026
+Date: 12/02/2026
 
 Author: Nicholas Young
 """
 
 import math
+import numpy as np
+
+
+class vector:
+   type = 'vector'
+
+   def __init__(self, xval, yval, zval):
+
+      self._xval = xval
+
+      self._yval = yval
+
+      self._zval = zval
+
+   def magnitude(self):
+
+      return np.abs(xval,yval,zval)
+
+v1 = vector(3, 5, 6)
+
+print(f"Vector is of magnitude: {v1.magnitude()})
+
