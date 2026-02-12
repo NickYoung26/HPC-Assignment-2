53dd4e374693d3171c8ac010c11cddffd97f4257
Nicholas Young <qdb21131@archie-w.hpc.strath.ac.uk>
2026-02-12 18:12:09 +0000

    Sbatch files were copied from the previous assignment, so we make sure not to lose the notation required to run on wee-archie. An old python file was also copied over so the appropriate shebang line could be copied over to our new python file for this task.
    License and readme.md also added however the readme has no description
    currently.

--- OOPclasses.py
+++ OOPclasses.py
@@ -1,1 +1,13 @@
+#!/opt/software/anaconda/python-3.10.9/bin/python
+
+"""
+Version: Python 3.10.9
+
+Description: Optimising parralesisation of an integral calculation equal to pi.
+
+Date: 02/02/2026
+
+Author: Nicholas Young
+"""
+
 import math
