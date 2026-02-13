5be353c7b85acc2073c63568240ccd0a059c69f1
Nicholas Young <qdb21131@archie-s.hpc.strath.ac.uk>
2026-02-13 20:42:40 +0000

    Sucessfully implemented a class 'Vector' which can have a magnitude and direction.

--- OOPclasses.py
+++ OOPclasses.py
@@ -10,26 +10,48 @@ Date: 12/02/2026
 Author: Nicholas Young
 """
 
-import math
+iimport math
 import numpy as np
 
 
 class vector:
-   type = 'vector'
+   type = 'Vector'
 
    def __init__(self, xval, yval, zval):
-
-      self._xval = xval
-
-      self._yval = yval
-
-      self._zval = zval
-
+       
+       self.xval = xval
+       self.yval = yval
+       self.zval = zval
+       
    def magnitude(self):
 
-      return np.abs(xval,yval,zval)
+      mag = np.sqrt((self.xval*self.xval) + (self.yval*self.yval) + (self.zval*self.zval))
+      
+      return mag
+  
+   def unitvectors(self):
+       
+       mag = self.magnitude()
+       
+       return (self.xval / mag, self.yval / mag, self.zval / mag)
+    
+   def direction(self):
+       
+       ux, uy, uz = self.unitvectors()
+       
+       alpha = math.degrees(math.acos(ux))
+       beta = math.degrees(math.acos(uy))
+       gamma = math.degrees(math.acos(uz))
+       
+       return alpha, beta, gamma
 
 v1 = vector(3, 5, 6)
 
-print(f"Vector is of magnitude: {v1.magnitude()})
+print(f"Vector Coords are: {v1.xval, v1.yval, v1.zval}") 
+print(f"Vector is of magnitude: {v1.magnitude():.3f}")
 
+ux1, uy1, uz1 = v1.unitvectors()
+print(f"Unit Vectors are (x, y, z): {ux1, uy1, uz1}")
+
+alpha1, beta1, gamma1 = v1.direction()
+print(f"Vector is of direction: {alpha1, beta1, gamma1} degrees")
